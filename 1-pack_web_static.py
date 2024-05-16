d2c0b9fb006a1881b43aef82169d94a924243820
ronaldkinyuru <ronaldkinyuru@gmail.com>
2024-05-17 01:48:19 +0300

    fabric3

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,15 +9,15 @@ from fabric.api import *
 
 
 def do_pack():
-        """
-            making an archive on web_static folder
-                """
+    """
+    making an archive on web_static folder
+    """
 
-                    time = datetime.now()
-                        archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-                            local('mkdir -p versions')
-                                create = local('tar -cvzf versions/{} web_static'.format(archive))
-                                    if create is not None:
-                                                return archive
-                                                else:
-                                                            return None
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
